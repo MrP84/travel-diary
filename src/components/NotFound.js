2d31c787c9fb3145e7296ad61a66f0293d1ce9ef
Guillaume <guillaume.provenzano@archriss.com>
2020-01-28 09:31:33 +0100

    initial commit

---
+++ src/components/NotFound.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const NotFound = () => <h2 className='notFound'>Penser à faire une 404... parce que même ça on n'en trouve pas !</h2>
+
+export default NotFound
---
+++ src/places.js
@@ -0,0 +1,29 @@
+// Une liste de places pour remplir directement ta boîte !!
+const places = {
+  place1: {
+    nom: 'Bali',
+    image: 'bali.jpg',
+    pays: 'Indonésie',
+    description: "Bali est une île d'Indonésie située entre les îles de Java et de Lombok. Elle fait partie des petites îles de la Sonde. Administrativement, l'île fait partie de la province du même nom et accueille sa capitale (ibu kota), Denpasar, dans le sud de l'île."
+  },
+  place2: {
+    nom: 'Koh Samui',
+    image: 'koh-samui.jpg',
+    pays: 'Thaïlande',
+    description: "Ko Samui, écrit parfois en translittération anglaise Koh Samui ou simplement Samui est une île du golfe de Thaïlande située dans la partie sud de l'isthme de Kra et appartenant à la province de Surat Thani."
+  },
+  place3: {
+    nom: 'Lagos',
+    image: 'lagos.jpg',
+    pays: 'Portugal',
+    description: "Lagos (littéralement  \"Les lacs\") est une ville et une agglomération située à l'embouchure de la Bensafrim au sud de l'Algarve, au Portugal."
+  },
+  place4: {
+    nom: 'Hoseshoe Bend',
+    image: 'horseshoe-bend.jpg',
+    pays: 'USA',
+    description: "Le Horseshoe Bend est le nom d'un méandre du fleuve Colorado situé à 6 km au sud de la ville de Page en Arizona et correspondant à un ancien plateau érodé."
+  }
+}
+
+export default places
