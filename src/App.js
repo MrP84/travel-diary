4a059896054ad715e35fdc72494b004b5142727b
Guillaume <guillaume.provenzano@archriss.com>
2020-01-28 11:15:53 +0100

    affichage des noms de pays

--- src/App.js
+++ src/App.js
@@ -2,21 +2,30 @@ import React, { Component } from 'react'
 // CSS
 import './App.css'
 import Header from "./components/Header";
+import Admin from "./components/Admin";
+import Card from "./components/Card";
+import places from "./places";
 
 class App extends Component {
   state = {
-    pseudo: this.props.match.params.pseudo
-  }
+    pseudo: this.props.match.params.pseudo,
+    places: {}
+  };
+
+  loadExample = () => this.setState({places});
+
 
   render () {
+    const cards = Object.keys(this.state.places).map(index => <Card key={index} details={this.state.places[index]} />);
+    console.log(cards);
+    console.log(this.state.places['place1']);
     return (
       <div className='box'>
         <Header pseudo={this.state.pseudo} />
         <div className='cards'>
-          <div className='card'>
-            <h2>Une Carte</h2>
-          </div>
+          { cards }
         </div>
+        <Admin loadExample={this.loadExample} />
       </div>
     )
   }
