4ac253ed5b301f5bd13bb90125b15365a1e37109
Guillaume <guillaume.provenzano@archriss.com>
2020-01-30 15:55:47 +0100

    ajout du bouton pour authtification via Github

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import propTypes from 'prop-types'
 
-const Login = ({ googleAuthenticate, facebookAuthenticate }) => {
+const Login = ({ googleAuthenticate, facebookAuthenticate, githubAuthenticate }) => {
   return (
     <div className="login">
       <h2>Connecte toi pour accéder à ton journal</h2>
-      <button onClick={googleAuthenticate} className="google-button">Je me connecte avec Google</button>
-      <button onClick={facebookAuthenticate} className="facebook-button">Je me connecte avec Facebook</button>
+      <button onClick={googleAuthenticate} className="login-button google-button">Je me connecte avec Google<i
+  className="fa fa-google" aria-hidden="true"/></button>
+      <button onClick={facebookAuthenticate} className="login-button facebook-button">Je me connecte avec Facebook<i
+  className="fa fa-facebook" aria-hidden="true"/></button>
+      <button onClick={githubAuthenticate} className="login-button github-button">Je me connecte avec Github<i
+  className="fa fa-github" aria-hidden="true"/></button>
     </div>
   )
 }
 
 Login.propTypes = {
   googleAuthenticate: propTypes.func.isRequired,
-  facebookAuthenticate: propTypes.func.isRequired
+  facebookAuthenticate: propTypes.func.isRequired,
+  githubAuthenticate: propTypes.func
 }
 
 export default Login
